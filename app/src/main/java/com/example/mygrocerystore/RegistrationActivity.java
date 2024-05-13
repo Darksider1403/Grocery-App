d47936b61868b50a1ea2bb001c2a4cb0a040c35c
quocthai2612003 <21130502@st.hcmuaf.edu.vn>
2024-05-13 20:34:04 +0700

    update

--- app/src/main/java/com/example/mygrocerystore/RegistrationActivity.java
+++ app/src/main/java/com/example/mygrocerystore/RegistrationActivity.java
@@ -2,17 +2,32 @@ package com.example.mygrocerystore;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.mygrocerystore.models.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
     Button signUp;
     EditText name, email, password;
     TextView signIn;
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -20,6 +35,9 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_registration);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         signUp = findViewById(R.id.reg_btn);
         name = findViewById(R.id.name);
@@ -33,7 +51,48 @@ public class RegistrationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                createUser();
             }
         });
+    }
+
+    private void createUser() {
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(userName)){
+            Toast.makeText(this,"Name is empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this,"Email is empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this,"Password is empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (userPassword.length()<6){
+            Toast.makeText(this,"Your password length need to be greater than 6 letter ",Toast.LENGTH_SHORT).show();
+        }
+        //Create user
+        auth.createUserWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    UserModel userModel = new UserModel(userName,userEmail,userPassword);
+                    String id = task.getResult().getUser().getUid();
+                    database.getReference().child("Users").child(id).setValue(userModel);
+
+                    Toast.makeText(RegistrationActivity.this,"Register success",Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(RegistrationActivity.this,"Error"+task.getException(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });{
+
+        }
     }
 }
