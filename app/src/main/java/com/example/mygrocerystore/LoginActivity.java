88f351b6518cac94c3d7a7daf32a5a1f82681c0e
quocthai2612003 <21130502@st.hcmuaf.edu.vn>
2024-05-13 21:37:57 +0700

    update

--- app/src/main/java/com/example/mygrocerystore/LoginActivity.java
+++ app/src/main/java/com/example/mygrocerystore/LoginActivity.java
@@ -1,23 +1,35 @@
 package com.example.mygrocerystore;
 
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     Button signIn;
     EditText email, password;
     TextView signUp;
 
+    FirebaseAuth auth ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
         signIn = findViewById(R.id.login_btn);
         email = findViewById(R.id.email_login);
         password = findViewById(R.id.password_login);
@@ -29,8 +41,40 @@ public class LoginActivity extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+        loginUser();
+        
             }
         });
+    }
+
+    private void loginUser() {
+
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+
+        if (TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(this, "Email is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPassword)) {
+            Toast.makeText(this, "Password is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (userPassword.length() < 6) {
+            Toast.makeText(this, "Your password length need to be greater than 6 letter ", Toast.LENGTH_SHORT).show();
+        }
+        //Login user
+        auth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this,"Login successfull",Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(LoginActivity.this,"Error"+task.isSuccessful(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
